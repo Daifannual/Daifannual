c9bbda4b9d2259309cbf404139a1c0a2490f6f29
Daifannual <daifannuramali@gmail.com>
2024-07-23 19:47:31 +0700

    pertama

---
+++ C#/latihan2.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("======================");
+            Console.WriteLine("Nama : Daifan Nur Amali");
+            Console.WriteLine("Kelas : XI RPL 1");
+            Console.WriteLine("No Absen : 3");
+            Console.WriteLine("======================");
+            Console.WriteLine("");
+            Console.WriteLine("Hobi saya = Bermain Game");
+            Console.WriteLine("======================");
+        }
+    }
+}
